003fa0e841f3cb793e2aa722f304f39e5a642233
Chris Mountford <chris@atlassian.com>
2013-12-05 18:36:26 +1100

    source sourceme not functions directly

--- sourceme.sh
+++ sourceme.sh
@@ -1,1 +1,8 @@
-alias gs='git status'
+if [[ -z "$CHRISTOSH_HOME" ]]; then
+    echo You need to define \$CHRISTOSH_HOME
+else 
+    . $CHRISTOSH_HOME/functions.sh
+
+    #aliases
+    alias gs='git status'
+fi
