58e34b205275852c1536ea4a0637933a444681cf
cmountford <chris.mountford@digitalasset.com>
2017-02-13 19:14:46 +1100

    added vpn connect commands

--- functions.sh
+++ functions.sh
@@ -147,4 +147,31 @@ mcd() {
     mkdir -p "$1" && cd "$1"
 }
 
+function vpn-connect {
+/usr/bin/env osascript <<-EOF
+tell application "System Events"
+        tell current location of network preferences
+                set VPN to service "AWS VPN" 
+                if exists VPN then connect VPN
+                repeat while (current configuration of VPN is not connected)
+                    delay 1
+                end repeat
+        end tell
+end tell
+EOF
+}
+
+function vpn-disconnect {
+/usr/bin/env osascript <<-EOF
+tell application "System Events"
+        tell current location of network preferences
+                set VPN to service "AWS VPN" 
+                if exists VPN then disconnect VPN
+        end tell
+end tell
+return
+EOF
+}
+alias vpnu='vpn-connect'
+alias vpnd='vpn-disconnect'
 alias remotes='git remote -v'
