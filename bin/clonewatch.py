17deb640b1830e724954e23fb892be3005d75876
Chris Mountford <chris@think.net.au>
2023-02-09 12:31:30 +1100

    can derive git url from github url now

--- bin/clonewatch.py
+++ bin/clonewatch.py
@@ -16,44 +16,75 @@ import sys
 import time
 
 # TODO support https urls, gitlab, bitbucket
-# TODO if the repo is already on disk, pull from remote instead of clone
+# TODO support deriving the git url from a github browser url
 # TODO add ThreadPoolExecutor concurrency from concurrent.futures see: 
+# NOTE: url parts are not decoded because they need to become dirs
 # https://docs.python.org/dev/library/concurrent.futures.html#threadpoolexecutor-example
 
 INTERVAL=1.5
 PARSE_REPO_URL= r'^(?:https://|git@)git(?:hu|la)b\.com:([^/]+)/(.*?)(\.git)?$'
+PARSE_GITHUB_URL = r'^https://github\.com/([^/]+)/([^/]+)$'
 CHECKOUT_BASEDIR=expanduser("~/src/other/")
 LOG=os.path.join(CHECKOUT_BASEDIR, "clonewatch.log")
+
+# parse URLs that are for browser visiting
+# like: https://github.com/under4mhz/microbee
+def parse_github_url(s):
+    return re.search(PARSE_GITHUB_URL, s)
 
 def parse_repo(s):
     return re.search(PARSE_REPO_URL, s)
 
-def update_console(num_running):
-    print("   \x1b[2Kdownloads active: {}".format(num_running), end='\r')
+# constructs a github git repo URL from user and reponame
+def github_repo(user, reponame):
+    return "git@github.com:{}/{}.git".format(user, reponame)
+
+def update_console(num_running, latest_repo):
+    print("\x1b[1F   latest: {}/{}             ".format(latest_repo[0], latest_repo[1]), end='\n')
+    print("   downloads active: {}             ".format(num_running), end='\r')
+
+def git_pull(repo_dir):
+    return Popen(["git", "-C", repo_dir, "pull", "-q"], stdout=DEVNULL, stderr=DEVNULL)
+
+def git_clone(url, repo_dir):
+    return Popen(["git", "clone", "-q", url, repo_dir], stdout=DEVNULL, stderr=DEVNULL)
 
 def main():
-    print("check log file at {}".format(LOG))
+    print("check log file at {}\n".format(LOG))
     logging.basicConfig(filename=LOG, format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
     logging.info("startup")
     previous_clipboard = ""
     gits=[]
+    latest = ("?", "?")
     while True:
         url = pyperclip.paste().strip()
         if len(url) > 0 and url != previous_clipboard: 
             previous_clipboard = url
             parsed = parse_repo(url)
+            if not parsed:
+                # try as github front-end url
+                parsed = parse_github_url(url)
+                if parsed:
+                    # rewrite URL as git
+                    user, repo = parsed.group(1, 2)
+                    url = github_repo(user, repo)
+                    logging.info("interpreting front-end URL as {}".format(url))
             if parsed:
                 user, repo = parsed.group(1, 2)
+                latest = (user, repo)
                 # the following can fail, but we probably want to die in that case
                 user_basedir = os.path.join(CHECKOUT_BASEDIR, user)
                 os.makedirs(name=user_basedir, mode=0o755, exist_ok=True)
                 repo_dir = os.path.join(user_basedir, repo)
                 if os.path.exists(repo_dir):
-                    logging.warning("repo dir {} exists, not cloning {}".format(repo_dir, url))
+                    logging.warning("pull existing {} assuming {}".format(repo_dir, url))
+                    # maybe check this ^ with git remote -v
+                    gits.append(git_pull(repo_dir))
                 else:
-                    gits.append(Popen(["git", "clone", "-q", url, repo_dir], stdout=DEVNULL, stderr=DEVNULL))
+                    gits.append(git_clone(url, repo_dir))
                     logging.info("cloning {} by {}: {}".format(repo, user, url))
             else:
+
                 logging.warning("not parse {}".format(url))
 
         time.sleep(INTERVAL)
@@ -66,7 +97,7 @@ def main():
                 logging.info("fail code {} for {}".format(x.poll(), x.args))
         # remove completed processes
         gits = [x for x in gits if x.poll() == None]
-        update_console(len(gits))
+        update_console(len(gits), latest)
 
 
 
