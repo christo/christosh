2139560c9091c9cccf630bc98379aad3742df913
Chris Mountford <chris@think.net.au>
2024-05-05 20:12:31 +1000

    small bug and improvements

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
-#!usr/bin/env zsh
+#!/usr/bin/env zsh
 
+# installs dotfiles
+# TODO research stow and decide if it's better
+#
+
+read -rq "GO_AHEAD?This script will overwrite local machine configuration files, OK? y/n"
+if [[ "$GO_AHEAD" != "y" ]]; then
+    echo ok exiting
+    exit
+fi
+
+echo "making local changes"
+echo temp exiting
+exit 1
 # machine install one-time config
 #
 
-if [[ `uname` == 'Darwin' ]]; then
-    $CHRISTOSH_HOME/mac-setup.sh
+if [[ $(uname) == 'Darwin' ]]; then
+    "$CHRISTOSH_HOME"/mac-setup.sh
 fi
 
-# install dotfiles
+# install dotfiles, asking for confirmation
 
 cp -i dotfiles/.tmux.conf ~/
