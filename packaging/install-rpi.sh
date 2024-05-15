c44384e2f3fd3d295eaa7843a7e94d880777e1ce
Chris Mountford <chris@think.net.au>
2024-05-15 10:29:05 +1000

    added a few more rpi install details

--- packaging/install-rpi.sh
+++ packaging/install-rpi.sh
@@ -3,4 +3,13 @@
 sudo apt update
 
 # minimal
-sudo apt install -y neovim zsh neofetch
+sudo apt install -y git neovim zsh neofetch
+
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+mkdir -p ~/src/christo
+
+echo TODO: add public key to ~/.ssh/authorized_keys
+echo TODO: clone or pull https://github.com/christo/christosh.git to ~/src/christo
