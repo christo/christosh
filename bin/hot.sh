c520ea17f02af48c195c246b1c46a9c793408ab3
Chris Mountford <chris@think.net.au>
2024-05-16 13:23:40 +1000

    tweak temp output

--- bin/hot.sh
+++ bin/hot.sh
@@ -5,5 +5,5 @@
 echo -n "GPU "
 # trim everything after last temp digit 
 vcgencmd measure_temp | perl -pe 's/(.*\d).*/\1/'
-cpu=$(</sys/class/thermal/thermal_zone0/temp) | perl -pe 's/(.*\d).*/\1/'
-echo "CPU temp=$((cpu/1000)) c"
+cpu=$(</sys/class/thermal/thermal_zone0/temp) 
+echo "CPU temp=$((cpu/1000)) c" | perl -pe 's/(.*\d).*/\1/'
