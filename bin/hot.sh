3885b1bdeccf56788b0ccb23785455bd453b4041
Chris Mountford <chris@think.net.au>
2024-05-16 13:22:17 +1000

    tweak temp output

--- bin/hot.sh
+++ bin/hot.sh
@@ -3,6 +3,7 @@
 # raspberry pi temperatures
 # TODO detect os, hardware, use corresponding commands
 echo -n "GPU "
-vcgencmd measure_temp
-cpu=$(</sys/class/thermal/thermal_zone0/temp)
+# trim everything after last temp digit 
+vcgencmd measure_temp | perl -pe 's/(.*\d).*/\1/'
+cpu=$(</sys/class/thermal/thermal_zone0/temp) | perl -pe 's/(.*\d).*/\1/'
 echo "CPU temp=$((cpu/1000)) c"
