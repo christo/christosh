a3c2df8a75d519ab7d720a275d7c87f4cbaa4659
Chris Mountford <chris@think.net.au>
2024-05-27 11:02:15 +1000

    temp linux package install script

---
+++ packaging/install-linux.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh 
+
+set -vx -o
+
+sudo apt-get -y install \
+haskell-stack \
+cabal-install \
+clang \
+rlwrap \
+curl \
+tfortune \
+hlint \
+neofetch \
+neovim \
+nodejs \
+syncthing \
+yt-dlp \
+ripgrep \
+bat \
+docker \
+fonts-jetbrains-mono \
+fonts-firacode \
+vino
