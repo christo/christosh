7453f7a3b9f63ee3c437a58475c6717b916bdc94
Chris Mountford <chris.mountford@gmail.com>
2024-12-17 15:39:28 +1100

    fixed command for splitting tmux with two commands

--- bin/tmux-split.sh
+++ bin/tmux-split.sh
@@ -1,31 +1,34 @@
 #!/usr/bin/env zsh
 
-# using tmux session named cw
-# start clonewatch with its logs tailing in two panes of a new tmux window
-# if such a session exists, attach to it, otherwise create it
-
-set -eu
 
 split_top=$1; shift
 split_bottom=$1; shift
 
-# create new session 
-# -u means utf8 -2 means 256 colour
-# -t foo:bar sets target session "foo" new window "bar"
-# -d start detached (will attach at end)
+created_new_session=false
+
 if tmux display-message -p '#S' > /dev/null 2>&1; then
-    current_session=$(tmux display-message -p '#S')
-    echo "reusing current session: $current_session"
+    sesh=$(tmux display-message -p '#S')
+    echo "reusing current session: $sesh"
 else
-    current_session="happygas"
-    echo "creating new session : $current_session"
-    tmux -Au2 new-session -n "$current_session" -d
+    sesh="happygas"
+    echo "creating new session : $sesh"
+    tmux -Au2 new-session -n "$sesh" -d
+    created_new_session=true
 fi
-tmux select-window -t "${current_session}:split_top"
-tmux send-keys -t "${current_session}:split_top" "split_top" Enter
-# vertical split (top and bottom panes), new pane receives focus
+
+window_index=$(tmux new-window -t "$sesh" -P -F "#{window_index}")
 tmux split-window -v
-tmux send-keys -t "${current_session}:split_bottom" "$split_bottom" Enter
+
+top_pane="$sesh:$window_index.1"
+bottom_pane="$sesh:$window_index.2"
+
+tmux send-keys -t "$top_pane" "$split_top" Enter
+tmux send-keys -t "$bottom_pane" "$split_bottom" Enter
+
 tmux select-layout even-vertical
-# now attach to it
-tmux -u2 attach -t "${current_session}"
+
+# Only attach if we created a new session and we're not already in tmux
+if [[ "$created_new_session" = true ]] && [[ -z "$TMUX" ]]; then
+    echo attaching
+    tmux -u2 attach -t "${sesh}"
+fi
